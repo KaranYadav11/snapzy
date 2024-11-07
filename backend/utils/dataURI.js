621817c7e3f846ec848ebeda0bba2a7159358028
KaranYadav11 <11yadavkaran@gmail.com>
2024-11-07 09:45:57 -0600

    Ready for deployment

--- backend/utils/dataURI.js
+++ backend/utils/dataURI.js
@@ -2,7 +2,7 @@ import DataUriParser from "datauri/parser.js";
 import path from "path";
 
 const parser = new DataUriParser();
-
+// new file
 const getDataUri = (file) => {
   return parser.format(path.extname(file.originalname).toString(), file.buffer)
     .content;
