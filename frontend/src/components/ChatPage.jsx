9809a9760ca5c3bcc2142079d011eb484199b7c6
Karan Yadav <126859195+KaranYadav11@users.noreply.github.com>
2024-11-09 09:35:40 -0600

    Update ChatPage.jsx

--- frontend/src/components/ChatPage.jsx
+++ frontend/src/components/ChatPage.jsx
@@ -23,7 +23,7 @@ const ChatPage = () => {
   const sendMessageHandler = async (receiverId) => {
     try {
       const res = await axios.post(
-        `https://snapzy.onrender.com/message/send/${receiverId}`,
+        `https://snapzy.onrender.com/api/v1/message/send/${receiverId}`,
         { textMessage },
         {
           headers: {
