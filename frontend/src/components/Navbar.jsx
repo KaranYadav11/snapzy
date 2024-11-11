fd6e2b910098574fb937eca06126e0fac7e01559
Karan Yadav <126859195+KaranYadav11@users.noreply.github.com>
2024-11-11 04:17:54 -0600

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,15 +4,16 @@ import { LogOut } from "lucide-react";
 function Navbar() {
   const logout = useLogout();
   return (
-    <div className="fixed w-full  z-50 h-12 justify-between px-2 flex items-center md:hidden top-0 bg-black text-lato text-white pb-2 font-bold">
-      <div className="bg-gradient-to-br  from-purple-700 via-pink-500 to-red-400 font-pacifico font-bold flex items-center tracking-wide bg-clip-text w-1/4 h-full text-transparent text-[23px]">
+    <div className="fixed w-screen z-50 h-14 justify-between px-2 flex items-center md:hidden top-0 bg-purple-200 text-lato text-white pb-2 font-bold">
+      <div className="bg-gradient-to-br from-purple-700 via-pink-500 to-red-400 font-pacifico font-bold flex items-center tracking-wide bg-clip-text w-1/4 h-full text-transparent text-[27px]">
         Snapzy
       </div>
-      <div onClick={logout} className="flex items-center">
+      <div onClick={logout} className="">
         <LogOut size={24} />
       </div>
     </div>
   );
 }
+
 
 export default Navbar;
